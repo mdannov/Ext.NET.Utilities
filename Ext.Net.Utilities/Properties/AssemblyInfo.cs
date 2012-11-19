20ce37b016b3de355ee1a93d3e61e2b48a9febdc
Geoffrey McGill <geoff@ext.net>
2012-11-18 22:04:12 -0700

    Updated to v2.1.0
    Signed-off-by: Geoffrey McGill <geoff@ext.net>

--- Ext.Net.Utilities/Properties/AssemblyInfo.cs
+++ Ext.Net.Utilities/Properties/AssemblyInfo.cs
@@ -4,14 +4,17 @@ using System.Runtime.InteropServices;
 using System.Security;
 
 [assembly: AssemblyCompany("Ext.NET, Inc.")]
-[assembly: AssemblyCopyright("Copyright © 2007-2012 Ext.NET, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2006-2012 Ext.NET, Inc.")]
+
+[assembly: AssemblyVersion("2.1.0")]
 [assembly: AssemblyTitle("Ext.Net Utilities")]
 [assembly: AssemblyProduct("Ext.Net.Utilities")]
 [assembly: AssemblyDescription("A .NET Utility Class Library")]
+
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: CLSCompliantAttribute(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("a7ac1696-f38d-49a7-822f-1872c4832c83")]
-[assembly: AssemblyVersion("2.0.1.*")]
 [assembly: AllowPartiallyTrustedCallers]
